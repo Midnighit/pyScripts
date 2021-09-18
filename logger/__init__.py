1a4c7501f9664f877ec722651ce54e3d28e863e0
Midnighit <alexeuler.ffm@gmail.com>
2021-09-18 15:01:19 +0200

    updated get_logger function

--- logger/__init__.py
+++ logger/__init__.py
@@ -1,12 +1,21 @@
-import os, logging
+import os, logging, sys
 from logging.handlers import RotatingFileHandler
 
-def get_logger(filename, loglevel=logging.INFO):
+def get_logger(filename, loglevel=logging.INFO, echo=True):
     logger = logging.getLogger(__name__)
     if not os.path.exists('logs'):
         os.mkdir('logs')
+
+    logger.setLevel(loglevel)
+
+    if echo:
+        prn_handler = logging.StreamHandler(sys.stdout)
+        prn_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
+        prn_handler.setLevel(logging.DEBUG)
+        logger.addHandler(prn_handler)
+
     file_handler = RotatingFileHandler('logs/' + filename, maxBytes=1048576, backupCount=3)
     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
-    logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
     logger.addHandler(file_handler)
     return logger
